dab8cd0f58019c23e668e751c1ff907bbb24a9d8
Alexander Miranda <alex.miranda@noaa.gov>
2016-09-28 13:42:12 -0700

    Added answers for fibonacci algorithms and supplementary code executing the code provided

---
+++ week1/code/fibonacci.py
@@ -0,0 +1,22 @@
+def fib_recur(n):
+   if n == 0:
+      return 0
+   elif n == 1:
+      return 1
+   else:
+      return fib_recur(n - 1) + fib_recur(n - 2)
+
+def fib_iter(n):
+   cur_term = 0
+   next_term = 1
+   total = 0
+
+   for k in range(0, n):
+      total = cur_term + next_term
+      next_term = cur_term
+      cur_term = total
+
+   return total
+
+print(fib_iter(10))
+print(fib_recur(10))
