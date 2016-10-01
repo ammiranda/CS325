5b67203d1372109378579b9a6b8eb49e94eadafc
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-01 11:12:06 -0700

    Added fibonacci_plot to plots

---
+++ week1/plots/fibonacci_plot.py
@@ -0,0 +1,9 @@
+import math
+import matplotlib.pyplot as plt
+
+x_vals = [5, 10, 15, 20, 30]
+
+plt.plot(xvals, [0.000017, 0.000086, 0.000861, 0.008358, 0.845506])
+plt.plot(xvals, [0.000014, 0.000006, 0.000005, 0.000005, 0.000008])
+plt.grid(True)
+plt.show()
