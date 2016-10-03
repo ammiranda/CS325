9ef4daf998072e99e0a007c96e4ac92c4a46d8c3
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-02 19:25:59 -0700

    Added updates to the plotting of the iterative fibonacci, added images of the big iterative plotting with and without fit line

--- week1/plots/fibonacci/iter.py
+++ week1/plots/fibonacci/iter.py
@@ -1,21 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import numpy.polynomial.polynomial as poly
+import time
 
-x_vals = np.array([x for x in range(5, 31)])
-y_vals = np.array([0.005, 0.004, 0.003, 0.004, 0.004, 0.005, 0.005, 0.005, 0.005, 0.005, 0.006, 0.005, 0.005, 0.005, 0.006, 0.006, 0.005, 0.006, 0.006, 0.005, 0.006, 0.007, 0.007, 0.007, 0.008, 0.007])
+def fib_iter(n):
+  cur_term = 0
+  next_term = 1
+  total = 0
+
+  for k in range(0, n):
+    total = cur_term + next_term
+    next_term = cur_term
+    cur_term = total
+  
+  return total
+
+def generate_runtimes(k, n):
+  times = []
+  for i in range(k, n + 1):
+    start_time = time.clock()
+    fib_iter(i)
+    times.append(1000 * (time.clock() - start_time))
+
+  return times
+
+y_vals_iter = generate_runtimes(5, 1000)
+
+x_vals = np.array([x for x in range(5, 1001)])
+y_vals = np.array(y_vals_iter)
 
 lin_coefs = poly.polyfit(x_vals, y_vals, 1)
 quad_coefs = poly.polyfit(x_vals, y_vals, 2)
 cube_coefs = poly.polyfit(x_vals, y_vals, 3)
+
+print(lin_coefs)
 
 lin_fit = poly.polyval(x_vals, lin_coefs)
 quad_fit = poly.polyval(x_vals, quad_coefs)
 cube_fit = poly.polyval(x_vals, cube_coefs)
 
 plt.plot(x_vals, lin_fit)
-plt.plot(x_vals, quad_fit)
-plt.plot(x_vals, cube_fit)
-plt.scatter(x_vals, y_vals)
+#plt.plot(x_vals, quad_fit)
+#plt.plot(x_vals, cube_fit)
+plt.plot(x_vals, y_vals, 'ko', markersize=2)
 plt.grid(True)
 plt.show()
