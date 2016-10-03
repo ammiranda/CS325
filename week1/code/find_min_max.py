06ca2cd2c0e612133b762a549c44024d4e2f7a2c
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-02 21:13:58 -0700

    Added comparisons counter to the find_min_max.py code

--- week1/code/find_min_max.py
+++ week1/code/find_min_max.py
@@ -1,5 +1,6 @@
 def find_min_max(arr):
     start_index = None
+    comparisons = 0
 
     if len(arr) % 2 == 0:
       if arr[0] > arr[1]:
@@ -9,6 +10,7 @@ def find_min_max(arr):
         max_val = arr[1]
         min_val = arr[0]              
       start_index = 2
+      comparisons += 1
     else:
       max_val = arr[0]
       min_val = arr[0]
@@ -21,14 +23,17 @@ def find_min_max(arr):
                 min_val = arr[i]
             if arr[i + 1] > max_val:
                 max_val = arr[i + 1]
+            comparisons += 2
         else:
             if arr[i + 1] < min_val:
                 min_val = arr[i + 1]
             if arr[i] > max_val:
                 max_val = arr[i]
+            comparisons += 2
                 
+    print(comparisons, len(arr))
     return {'max': max_val, 'min': min_val}
 
-example = find_min_max([9, 3, 5, 10, 1, 7, 12])
+example = find_min_max([40, 9, 3, 5, 10, 1, 7, 12])
 
 print("Max: %d Min: %d" % (example['max'], example['min']))
