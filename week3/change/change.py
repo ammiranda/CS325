565e40be720ee2be5b48fe8d317922171b9dc750
ammiranda <alexandermichaelmiranda@gmail.com>
2016-10-18 11:04:54 -0400

    Added more test cases to the change.py dynamic programming solution

--- week3/change/change.py
+++ week3/change/change.py
@@ -17,4 +17,6 @@ def make_change(coin_val_list, change, known_results):
 
 print(make_change([1, 3, 4], 11, [0]*64))
 print(make_change([1, 10, 25, 50], 40, [0]*64))
-    
+print(make_change([1, 5, 10, 25, 50], 75, [0]*100))
+print(make_change([1, 10, 21, 50], 63, [0]*100))
+print(make_change([1, 2, 4, 8, 16], 120, [0]*150))
